054f799d86a7869f12b78ecf35e7c33394919810
IljaAstasev <Ilja.astasev@ivkhk.ee>
2022-11-10 12:42:17 +0200

    PhP

---
+++ model/booksArray.php
@@ -0,0 +1,23 @@
+<?php
+$books = array(
+    array("bookname" => "HTML and CSS",
+            "author" => "Jon Duckett",
+            "price" => "17.19$",
+            "description" => "Every day, more and more people want to learn some HTML and CSS. Joining the professional web designers and programmers are new audiences who need to know a little bit of code at work (update a content management system or e-commerce store) and those who want to make their personal blogs more attractive. Many books teaching HTML and CSS are dry and only written for those who want to become programmers, which is why this book takes an entirely new approach.",
+            "image" => "31b4K-hFH-L._SX395_BO1,204,203,200_.jpg"),
+    array("bookname" => "Head First HTML5 Programming",
+            "author" => "Eric Freeman, Elisabeth Robson",
+            "price" => "35.35$",
+            "description" => "HTML has been on a wild ride. Sure, HTML started as a mere markup language, but more recently HTML's put on some major muscle. Now we've got a language tuned for building web applications with Web storage, 2D drawing, offline support, sockets and threads, and more. And to speak this language you've got to go beyond HTML5 markup and into the world of the DOM, events, and JavaScript APIs.",
+            "image" => "51KZBvF5+lL._AC_SY780_.jpg"),
+    array("bookname" => "Head First JavaScript Programming",
+            "author" => "Eric Freeman, Elisabeth Robson",
+            "price" => "37.60$",
+            "description" => "This brain friendly guide teaches you everything from JavaScript language fundamentals to advanced topics, including objects, functions, and the browser’s document object model. You won’t just be reading—you’ll be playing games, solving puzzles, pondering mysteries, and interacting with JavaScript in ways you never imagined. And you’ll write real code, lots of it, so you can start building your own web applications.",
+            "image" => "519dx5KHv-L._AC_SY780_.jpg"),
+    array("bookname" => "jQuery in Action",
+            "author" => "Bear Bibeault, Yehunda Katz",
+            "price" => "32.17$",
+            "description" => "jQuery in Action, Third Edition, is a fast-paced and complete guide to jQuery, focused on the tasks you'll face in nearly any web dev project.",
+            "image" => "411J9dsrKwL._SX397_BO1,204,203,200_.jpg"));
+?>
